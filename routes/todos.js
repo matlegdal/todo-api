9a4c8f745a2409d95abcd297fe87532b388929ee
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-05-18 10:27:01 -0400

    show todo route

--- routes/todos.js
+++ routes/todos.js
@@ -2,20 +2,33 @@ var express = require("express"),
     db = require("../models"),
     router = express.Router();
 
+// INDEX ROUTE
 router.get("/", function (req, res) {
     db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    });
+        .then(function (todos) {
+            res.json(todos);
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
 });
 
-router.post("/", function(req, res){
+// POST NEW TODO ROUTE
+router.post("/", function (req, res) {
     db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
+        .then(function (newTodo) {
+            res.status(201).json(newTodo);
+        })
+        .catch(function (err) {
+            res.send(err);
+        });
+});
+
+// SHOW ROUTE
+router.get("/:todoId", function (req, res) {
+    db.Todo.findById(req.params.todoId)
+    .then(function(foundTodo){
+        res.json(foundTodo);
     })
     .catch(function(err){
         res.send(err);
