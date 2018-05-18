6123522087bfd63fa9c91f1a1d74b7a06ca7a379
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-05-18 13:27:28 -0400

    add style for completed todos

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ require([
     "dojo/query",
     "dojo/request",
     "dojo/_base/array",
+    "dojo/NodeList-dom",
     "dojo/domReady!"
 ], function (dom, domConstruct, query, request, dojoArray) {
     request.get("/api/todos", {
@@ -12,11 +13,14 @@ require([
         .then(function addTodos(todos) {
             var list = query(".list")[0];
             dojoArray.forEach(todos, function (todo) {
+                var className = "task";
+                if (todo.completed) {
+                    className += " done";
+                }
                 domConstruct.create("li", {
                     innerHTML: todo.name,
-                    className: "task"
+                    className: className
                 }, list);
-
             });
         })
         .catch(function (err) {
