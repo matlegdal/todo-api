f0263fd2340a1134a28bbf832274946a21d0e4de
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-05-18 09:08:15 -0400

    add connection to mongodb

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var express = require("express"),
+const express = require("express"),
+    mongoose = require('mongoose'),
     app = express();
 
-app.get("/", function(req, res){
-    res.send("hi there");
+
+//Set up default mongoose connection
+mongoose.connect('mongodb://localhost:27017/todos-spa');
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+const db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Routes
+app.get("/", function (req, res) {
+    res.json({
+        message: "Hi from JSON"
+    });
 });
 
-app.listen(3000, "127.0.0.1", function(){
+// Start server
+app.listen(3000, "127.0.0.1", function () {
     console.log("Server started on 127.0.0.1:3000");
 });
