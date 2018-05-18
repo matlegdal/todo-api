e4d5a8bdf91e83fc901e9ea5eee4783d98df2ad1
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-05-18 10:17:35 -0400

    post new todo route

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require("express"),
+    bodyParser = require("body-parser"),
     app = express();
 
 var todoRoutes = require("./routes/todos");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use("/api/todos", todoRoutes);
 
 // Routes
 app.get("/", function (req, res) {
     res.send("Hello from the root route");
 });
-
-app.use("/api/todos", todoRoutes);
 
 // Start server
 app.listen(3000, "127.0.0.1", function () {
